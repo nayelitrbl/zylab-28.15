f42eddbb583b63569e915760e496481b8f1e6675
nayelitrbl <144268264+nayelitrbl@users.noreply.github.com>
2023-11-29 21:15:37 -0600

    Create PlaylistNode.cpp
    basic functions for PlaylistNode.cpp excluding the pointer functions

---
+++ PlaylistNode.cpp
@@ -0,0 +1,48 @@
+#include "PlaylistNode.h"
+#include <iostream>
+using namespace std;
+PlaylistNode::PlaylistNode(){
+   this->uniqueID = "none";
+   this->songName = "none";
+   this->artistName = "none";
+   this->songLength = 0;
+}
+
+PlaylistNode::PlaylistNode(string id, string sName, string aName, int sLen){
+   this->uniqueID = id;
+   this->songName = sName;
+   this->artistName = aName;
+   this->songLength = sLen;
+}
+
+string PlaylistNode::GetID(){
+   return uniqueID;
+}
+
+string PlaylistNode::GetSongName(){
+   return songName;
+}
+ 
+string PlaylistNode::GetArtistName(){
+  return artistName;
+}
+
+int PlaylistNode::GetSongLength(){
+   return songLength;
+}
+
+// PlaylistNode::PlaylistNode* GetNext(){
+// }
+
+// PlaylistNode::void InsertAfter(PlaylistNode* nodePtr){
+// }
+
+// PlaylistNode::void SetNext(PlaylistNode* nodePtr){
+// }
+
+void PlaylistNode::PrintPlaylistNode(){
+   cout << "Unique ID: " << uniqueID << endl;
+   cout << "Song Name: " << songName << endl;
+   cout << "Artist Name: " << artistName << endl;
+   cout << "Song Length (in seconds): " << songLength << endl;
+}
